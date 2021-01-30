f23e28f8679c132e26a66b87a52300a96afc9a07
buxpob <buxpob.xax@gmail.com>
2021-01-30 19:28:21 +0300

    Внёс правки во второе задание

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 function getRandomInt(min, max) {
-  if (min >= 0 && max > 0 && min < max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  } if (min >= max) {
-    alert('Минимальные значение должно быть меньше максимального!');
-  } else {
+  if (min < 0) {
     alert('Диапазон может быть только положительный, включая ноль!');
   }
+  if (min >= max) {
+    alert('Минимальные значение должно быть меньше максимального!');
+  }
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 getRandomInt(1, 3);
 
-function getRandomIntFloatPoint(min, max, floatPoint) {
-  if (min >= 0 && max > 0 && min < max) {
-    return +(Math.random() * (max - min) + min).toFixed(floatPoint);
-  } if (min >= max) {
-    alert('Минимальные значение должно быть меньше максимального!');
-  } else {
+function getRandomIntFloatPoint(min, max, floatPoint = 2) {
+  if (min < 0) {
     alert('Диапазон может быть только положительный, включая ноль!');
   }
+  if (min >= max) {
+    alert('Минимальные значение должно быть меньше максимального!');
+  }
+
+  return +(Math.random() * (max - min) + min).toFixed(floatPoint);
 }
 
-getRandomIntFloatPoint(1, 3, 5);
+getRandomIntFloatPoint(1, 3, 2);
